7c0b4a4b6a6700d53e650202468531df47ad76a8
[Josh Nguyen] <[joshnguyentoronto@gmail.com]>
2023-09-01 16:55:39 -0400

    complete problems

--- Heap - Priority Queue/Kth Largest Element In a Stream.py
+++ Heap - Priority Queue/Kth Largest Element In a Stream.py
@@ -24,3 +24,16 @@ kthLargest.add(4);   // return 8
 
 '''
 
+class KthLargest:
+    def __init__(self, k: int, nums: List[int]):
+        self.nums = nums
+        self.k = k
+        heapq.heapify(nums)
+        while len(self.nums) > self.k:
+            heapq.heappop(self.nums)
+
+    def add(self, val: int) -> int:
+        heapq.heappush(self.nums, val)
+        if len(self.nums) > self.k:
+            heapq.heappop(self.nums)
+        return self.nums[0]
